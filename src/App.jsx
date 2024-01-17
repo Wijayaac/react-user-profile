8a2eaee4d374ea6a9831a90cd1590c59f94d716d
Wijayaac <wijayaac25@gmail.com>
2024-01-18 06:00:58 +0800

    begin : add scaffolding react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className='w-full'>
+        <h1 className='text-base-100 text-xl'>Hello</h1>
+        <div className='avatar'>
+          <div className='w-full'>
+            <img src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
